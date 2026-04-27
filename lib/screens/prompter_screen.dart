4c15e9101ef4c8a3d324a964b05c8591dca82bcc
hichamozor <hichamozor2@gmail.com>
2026-04-27 17:58:50 +0200

    Fix: add Ticker import for Flutter 3.41

--- lib/screens/prompter_screen.dart
+++ lib/screens/prompter_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:camera/camera.dart';
 import 'package:provider/provider.dart';
